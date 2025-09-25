8b834467379cacac868a5c4723b11053efc8a9f5
jenny.jione.jang <jenny.jione.jang@gmail.com>
2025-09-25 17:21:16 +0900

    feat: 시작/종료 시각이 '~'인 경우 기본 시간으로 치환

--- preprocess.py
+++ preprocess.py
@@ -19,12 +19,22 @@ with open('timeblock.csv', 'r', encoding='utf-8') as f,\
         '수면':'sleep'
     }
 
+    day_start = '05:00'
+    day_end = '04:59'
+
     new_rows = []
     for row in rdr:
         # desc escape 처리
         row[-1] = row[-1].replace('\n', '\\n').replace('"', "\'")
         # type 변환
         row[-2] = categories.get(row[-2], 'other')
+
+        # 취침/기상 시각 ~ 변경
+        # row[1] = 시작시각, row[2] = 종료시각
+        if row[1] == '~':
+            row[1] = day_start
+        if row[2] == '~':
+            row[2] = day_end
 
         # key:value 쌍 생성
         new_row = [f'{key}:"{value}"' for key, value in zip(h, row)]
